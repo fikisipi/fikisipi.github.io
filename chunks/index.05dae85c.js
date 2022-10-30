8a96602d0de6723c35c4a23345bd44a7ce78e4d4
fikisipi <filipdimitrovski22@gmail.com>
2022-10-30 22:57:48 +0000

    Deploying to gh-pages from @ fikisipi/fikisipi.github.io@3b848e97296f0d553aa3d89ee16b0e5dbc4b3ece 🚀

---
+++ chunks/index.05dae85c.js
@@ -0,0 +1,1 @@
+import{s,m as e}from"../index.e8fe0098.js";s("/assets/style.module.7b16dcc3.css");const t="about_migxty";export default({query:s})=>e`<section class=${t}><h1>About</h1><p>A page all about this website.</p><pre>${JSON.stringify(s)}</pre></section>`;
--- chunks/index.c8b1c45f.js
+++
@@ -1,1 +0,0 @@
-import{s,m as t}from"../index.326b4c8d.js";s("/assets/style.module.7b16dcc3.css");const e="about_migxty";export default({query:s})=>t`<section class=${e}><h1>About</h1><p>A page all about this website.</p><pre>${JSON.stringify(s)}</pre></section>`;
