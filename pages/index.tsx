d150c120a37c82a11641f924d5a5262a61ab4559
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2024-08-04 02:10:00 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -138,8 +138,7 @@ export default function A(props: StaticProps) {
                 <path d="M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z" />
               </svg>
             </h1>
-            I'm interested in
-            machine learning and web development, as well prediction markets and startups.
+            I'm interested in computer and data science, as well prediction markets and startups.
             <div className="text-center my-8">
               <a href="https://fikisipi.substack.com/" className="inline-block text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2">
               <svg aria-hidden="true" className="inline w-4 h-4 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
